84a62e147f1a8e1164eed33dfb8f0bbc04e7c5a1
GabrieleTuzzo <gabriele.tuzzo@gmail.com>
2024-11-27 15:44:04 +0100

    ADD Card component and styles

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,24 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import Card from './components/Card/Card.jsx';
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <StrictMode>
+        <main>
+            <div class="container">
+                <div class="row">
+                    <div class="col-6">
+                        <Card></Card>
+                    </div>
+                    <div class="col-6">
+                        <Card></Card>
+                    </div>
+                    <div class="col-6">
+                        <Card></Card>
+                    </div>
+                </div>
+            </div>
+        </main>
+    </StrictMode>
+);
