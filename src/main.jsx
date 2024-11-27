f79b33b00efc750e6c0916422ce0d7e9de469ebf
GabrieleTuzzo <gabriele.tuzzo@gmail.com>
2024-11-27 15:54:04 +0100

    ADD footer component, content and styles

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
 import Card from './components/Card/Card.jsx';
+import Footer from './components/Footer/Footer.jsx';
 
 createRoot(document.getElementById('root')).render(
     <StrictMode>
@@ -20,5 +21,6 @@ createRoot(document.getElementById('root')).render(
                 </div>
             </div>
         </main>
+        <Footer></Footer>
     </StrictMode>
 );
